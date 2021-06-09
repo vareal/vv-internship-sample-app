20c07d5109d9515b7990b264f7bbe4d323f114b1
Hieu Nguyen <10509903+hieunguyentrung@users.noreply.github.com>
2021-06-09 14:58:03 +0700

    update

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -117,6 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
   config.action_mailer.smtp_settings = {
     :domain               => ENV['SMTP_DOMAIN'],
     :address              => ENV['SMTP_ADDRESS'],
