7945f7bc4362f736a9b9f6f2372be2f1daffb08c
Hieu Nguyen <10509903+hieunguyentrung@users.noreply.github.com>
2021-06-09 15:26:04 +0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      @user.update activated: true
       # @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Welcome"
       redirect_to root_path
     else
       render :new
